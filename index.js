ed18ba9e2f51737c84c9ebe02d4cef01c493bb65
bebopvinh <vinhdo07@gmail.com>
2019-05-21 12:38:57 -0700

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  let result
+  if (ride < 400) {
+    result =  "This one is on me!"
+  } else if (ride > 2000 && ride <= 2500) {
+    result = 'I will gladly take your thirty bucks.'
+  } else {
+    result = "No can do."
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let result
+
+  if (city === "NYC") {
+    result = "Ok, sounds good." 
+  } else {
+    result = "No go."
+  }
+
+  return result
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let result
+
+  if (tip === "generous") {
+    result = "Thank you so much."
+  } else if (tip === "not as generous") {
+    result = "Thank you."
+  } else {
+    result = "Bye."
+  }
+
+  return result
 }
